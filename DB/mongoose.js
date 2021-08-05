ebc177c1dc90d3fa0390fa7f84698ea9b92ffe94
Yoni <yonatansamfisher@gmail.com>
2021-08-05 12:26:06 +0300

    updated DB

--- DB/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 8080 ;
-const CONNECTION_URL= process.env.CONNECTION_URL ;
-
-mongoose.connect(`${CONNECTION_URL}dbSchool`,(error,dbResult)=>{
-    if (error) console.log(error);
-    
-})
---
+++ src/DB/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8080 ;
+const CONNECTION_URL= process.env.CONNECTION_URL ;
+
+const connectToDB = ()=>{
+return mongoose.connect(CONNECTION_URL,
+    {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+    })
+}
+
+connectToDB().then(()=>console.log('MongoDB Connected'))
+.catch(error=> 
+    console.error('Connection error',error.message)
+)
