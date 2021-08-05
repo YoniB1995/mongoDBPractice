fcb4d54cb890c2ac48c1c9e4295192cd2ee5b9ec
Yoni <yonatansamfisher@gmail.com>
2021-08-05 10:42:07 +0300

    updates

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8080 ;
+const mongodb = require('mongodb').MongoClient;
 const express = require('express');
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json()); // JSON יכולת לקרוא ולהציג מידע מ
+app.use(express.urlencoded({extended:true})); // params יכולת לשלוף מידע מ
 app.use(cors());
+
+mongodb.connect('mongodb://localhost:27017',{})
+
+
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is up one port:${PORT}`);
