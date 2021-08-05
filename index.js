f3a1a6e0d747f398b9137d7de7b0aa71dbaf340f
Yoni <yonatansamfisher@gmail.com>
2021-08-05 11:23:51 +0300

    updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8080 ;
+const CONNECTION_URL= process.env.CONNECTION_URL ;
 const mongodb = require('mongodb').MongoClient;
 const express = require('express');
 const cors = require("cors");
@@ -9,16 +10,31 @@ app.use(express.json()); // JSON יכולת לקרוא ולהציג מידע מ
 app.use(express.urlencoded({extended:true})); // params יכולת לשלוף מידע מ
 app.use(cors());
 
-mongodb.connect('mongodb://localhost:27017',{})
-
-
-
-
 
 app.listen(PORT, ()=>{
     console.log(`Server is up one port:${PORT}`);
 })
 
 app.get('/', (req,res)=>{
-    res.send("success")
+    connectToDB(res)
+    
 })
+
+
+const connectToDB = (resFromGet)=> {
+return mongodb.connect(`${CONNECTION_URL}dbSchool`,
+(error,dbResult)=>{
+    if (error) console.log(error);
+    const studentCollection = dbResult.db('dbSchool').collection('students');
+    const document = {"first_name":"yossi","last_name":"simha","age":29};
+    studentCollection.insertOne(document,(error,result)=>{
+        if (error) console.log(error);
+    resFromGet.send(result)
+        console.log(result);
+    });
+    // studentCollection.findOne({first_name:"dani"},(err,result)=>{
+    //     if (err) console.log(err);
+    //     resFromGet.send(result);
+    // })
+})
+}
